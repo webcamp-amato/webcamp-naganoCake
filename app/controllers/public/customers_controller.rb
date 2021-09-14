c257d18f093bae65faef73daba6f0a5eda58a8a5
mt60000 <niruyaku45@gmail.com>
2021-09-14 09:39:39 +0000

    コントローラー作成

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,24 @@
+class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def confirm
+  end
+
+
+  private
+
+    def customer_params
+    end
+end

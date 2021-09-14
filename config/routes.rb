dda8a7b6d0eb195cf5dd35be0c80b7c7691a6f1b
ko7ta7ko7ta@gmail.com <ec2-user@ip-172-31-27-240.ec2.internal>
2021-09-14 12:11:02 +0000

    ルート

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,32 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'orders/index'
-    get 'orders/show'
-    get 'orders/confirm'
+
+  resources :customers, only: [:show, :edit, :update, :destroy] do
+    get '/confirm' => 'customers#confirm', as: "confirm"
+    resources :delivery_addresses, only: [:index, :edit, :update, :destroy, :create]
+    resources :cart_items, only: [:create, :index, :update, :destroy]
+    delete '/cart_items' => 'cart_items#all_destroy'
+    resources :orders, only: [:create, :index, :show]
+    get '/orders/confirm' => 'orders#confirm'
+    get '/orders/complete' => 'orders#complete'
   end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
+
+  resources :items, only: [:index, :show]
+  get '/search' => "searches#search", as: "search"
+
+  root :to =>'homes#top'
+  get 'homes/about'
+
+
+  namespace :admin do
+    resources :admins, only: [:index, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :items, except: [:destroy]
+    resources :orders, only: [:show, :index, :update]
+    resources :order_items, only: [:update]
   end
-  namespace :public do
-    get 'carts/index'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'users/index'
-    get 'users/show'
-    get 'users/edit'
-  end
+
+
+
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

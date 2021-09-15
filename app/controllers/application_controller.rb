4cfd90c2347ad9eafdef251801dade471d8dfb31
Sugizou0215 <sugizou0215@gmail.com>
2021-09-15 08:51:37 +0000

    params修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,17 @@
 class ApplicationController < ActionController::Base
+  #ログインしていない場合はトップページとアバウトページのみ遷移可能
+  before_action :authenticate_user!, except: [:top, :about]
+
   #ログインしている場合は商品一覧ページへ
   def after_sign_in_path_for(resource)
     items_path
   end
 
+  protected
+
   # 新規登録の保存機能
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,
-      keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postcode, :address, :phone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postcode, :address, :phone_number])
 
       #sign_up時のパラメータ(メールアドレス)
   		devise_parameter_sanitizer.permit(:sign_in, keys: [:email])

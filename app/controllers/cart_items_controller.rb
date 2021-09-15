080d7ce73861ed6e83d9362ac6dcf69788d5978b
mt60000 <niruyaku45@gmail.com>
2021-09-15 14:13:23 +0000

    cart_item/controllerの記述

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,23 +2,36 @@ class CartItemsController < ApplicationController
   before_action :authenticate_customer!
 
   def index
+    @cart_items = current_customer.cart_items
   end
 
   def create
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    @cart_item.save
+    render 'cart_items/index'
   end
 
   def update
+    @cart_item = current_customer.cart_items.find(cart_item_params)
+    @cart_item.update
+    render request.referer
   end
 
   def destroy
+    @cart_item = current_customer.cart_items.find_by(params[:id])
+    @cart_item.destroy
+    render request.referer
   end
 
   def all_destroy
+    @cart_items = current_customer.cart_items
+    @cart_items.destroy
   end
 
 
   private
 
     def cart_item_params
+      params.require(:cart_item).permit(:customer, :item_id, :count)
     end
 end

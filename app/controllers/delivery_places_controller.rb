f80e7592db13ced47d0e769a326fb0f18fb79790
Sugizou0215 <sugizou0215@gmail.com>
2021-09-16 04:57:37 +0000

    DeliveryPlace周辺実装

--- app/controllers/delivery_places_controller.rb
+++ app/controllers/delivery_places_controller.rb
@@ -2,22 +2,39 @@ class DeliveryPlacesController < ApplicationController
    before_action :authenticate_customer!
 
   def index
+    @delivery_places = current_customer.delivery_places
+    @delivery_place = DeliveryPlace.new
   end
 
   def edit
+    @delivery_place = DeliveryPlace.find(params[:id])
   end
 
   def create
+    @delively_place = DeliveryPlace.new(delivery_params)
+    @delively_place.customer_id = current_customer.id
+    @delively_place.save
+    @delivery_places = current_customer.delivery_places #index遷移用
+    redirect_to delivery_places_path
   end
 
   def update
+    @delivery_place = DeliveryPlace.find(params[:id])
+    @delivery_place.update(delivery_params)
+    redirect_to delivery_places_path
   end
 
   def destroy
+    @delivery_place = DeliveryPlace.find(params[:id])
+    @delivery_place.destroy
+    @delivery_places = current_customer.delivery_places #index遷移用
+    redirect_to delivery_places_path
   end
 
   private
 
-    def delivery_params
-    end
+  def delivery_params
+    params.require(:delivery_place).permit(:postcode, :address, :addressee)
+  end
+
 end

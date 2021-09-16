3dd6172fbd9caf8c9e518e94fed4766a52728a2d
mt60000 <niruyaku45@gmail.com>
2021-09-16 08:45:58 +0000

    カート機能大まかに追加

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -6,32 +6,44 @@ class CartItemsController < ApplicationController
   end
 
   def create
+    @cart_items = current_customer.cart_items
     @cart_item = current_customer.cart_items.new(cart_item_params)
-    @cart_item.save
-    render 'cart_items/index'
+    @cart_item.customer_id = current_customer.id
+    if @cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.count += params[:cart_item][:count].to_i
+      @cart_item.save
+      redirect_to customer_cart_items_url(@cart_item.customer)
+    elsif
+      @cart_item.save
+      redirect_to customer_cart_items_url(@cart_item.customer)
+    else
+      redirect_to items_url
+    end
   end
 
   def update
-    @cart_item = current_customer.cart_items.find(cart_item_params)
-    @cart_item.update
+    @cart_item = current_customer.cart_items.find(params[:id])
+    @cart_item.update(cart_item_params)
     render request.referer
   end
 
   def destroy
     @cart_item = current_customer.cart_items.find_by(params[:id])
     @cart_item.destroy
-    render request.referer
+    redirect_to request.referer
   end
 
   def all_destroy
     @cart_items = current_customer.cart_items
     @cart_items.destroy
+    redirect_to items_url
   end
 
 
   private
 
     def cart_item_params
-      params.require(:cart_item).permit(:customer, :item_id, :count)
+      params.require(:cart_item).permit(:customer_id, :item_id, :count)
     end
 end

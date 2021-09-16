47d6fe2094e42cb83f5194a95fe76c4048971469
ko7ta7ko7ta@gmail.com <ec2-user@ip-172-31-27-240.ec2.internal>
2021-09-16 00:10:14 +0000

    new, create

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,11 +8,37 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @customer = Customer.find(params[:customer_id])
+    # 不要かも
+    @cart_items = @customer.cart_items
+    # 不要かも
+    @order.payment_method = params[:order][:payment_method]
+    
+    if params[:order][:place_option] == "0"
+      @order.postcode = @customer.postcode
+      @order.address = @customer.address
+      full_name = @customer.last_name + @customer.first_name
+      @order.addressee = full_name
+    elsif params[:order][:place_option] == "1"
+      @sta = params[:order][:order_address].to_i
+      @order_address = DeliveryPlace.find(@sta)
+      @order.postcode = @order_address.postcode
+      @order.address = @order_address.address
+      @order.addressee = @order_address.addressee
+    elsif params[:order][:place_option] == "2"
+      @order.postcode = params[:order][:postcode]
+      @order.address = params[:order][:address]
+      @order.addressee = params[:order][:addressee]
+    end
+    
   end
   
   def new
-    @customer = Customer.find(params[:id])
-    @new_place = Delivery_places.new
+    @customer = Customer.find(params[:customer_id])
+    @order = Order.new
+    @order.customer_id = @customer.id
+    @new_place = DeliveryPlace.new
+    @new_place.customer_id = @customer.id
     
   end
 
@@ -25,5 +51,6 @@ class OrdersController < ApplicationController
 
   private
     def order_params
+      params.require(:order).permit(:payment_method, :postcode, :address, :addressee)
     end
 end

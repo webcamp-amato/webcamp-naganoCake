bda871667c796c442650ce0673417bd8872c0733
ko7ta7ko7ta@gmail.com <ec2-user@ip-172-31-27-240.ec2.internal>
2021-09-16 12:57:19 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -11,15 +11,14 @@ Rails.application.routes.draw do
     registrations: 'customers/registrations'
   }
 
-  resources :customers, only: [:show, :edit, :update, :destroy] do
-    get '/confirm' => 'customers#confirm', as: "confirm"
-    resources :cart_items, only: [:create, :index, :update, :destroy]
-    delete '/cart_items' => 'cart_items#all_destroy'
-    post '/orders/confirm' => 'orders#confirm'
-    get '/orders/complete' => 'orders#complete'
-    resources :orders, only: [:create, :index, :show, :new]
-  end
-
+  resources :customers, only: [:show, :edit, :update]
+  patch '/leave' => 'customers#leave', as: "leave"
+  get '/confirm' => 'customers#confirm', as: "confirm"
+  resources :cart_items, only: [:create, :index, :update, :destroy]
+  delete '/cart_items' => 'cart_items#all_destroy'
+  post '/orders/confirm' => 'orders#confirm'
+  get '/orders/complete' => 'orders#complete'
+  resources :orders, only: [:create, :index, :show, :new]
   resources :delivery_places, only: [:index, :edit, :update, :destroy, :create]
   resources :items, only: [:index, :show]
   get '/search' => "searches#search", as: "search"
@@ -27,13 +26,13 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about'
 
-
   namespace :admin do
     resources :admins, only: [:index, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items, except: [:destroy]
     resources :orders, only: [:show, :index, :update]
     resources :order_items, only: [:update]
+    root to: 'homes#top'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

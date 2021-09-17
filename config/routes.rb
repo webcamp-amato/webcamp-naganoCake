6aa2b3991dd6bd5442875feb3de740e692d17f87
mt60000 <niruyaku45@gmail.com>
2021-09-17 08:48:08 +0000

    route

--- config/routes.rb
+++ config/routes.rb
@@ -37,6 +37,7 @@ Rails.application.routes.draw do
     resources :items, except: [:destroy]
     resources :orders, only: [:show, :index, :update]
     resources :order_items, only: [:update]
+    root to: 'homes#top'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

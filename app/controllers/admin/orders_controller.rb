d612c0f014d8febc27629b900e3183ec8f1a7f33
ko7ta7ko7ta@gmail.com <ec2-user@ip-172-31-27-240.ec2.internal>
2021-09-17 13:39:37 +0000

    kaminari,ステータス以外完了

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,11 +1,13 @@
 class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
 
-  def index
-  end
 
   def show
     @order = Order.find(params[:id])
+    @order_items = OrderItem.where(order_id: params[:id])
+    @sum = 0
+    @customer = @order.customer
+    
   end
 
   def update

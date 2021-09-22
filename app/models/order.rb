b270db0259bd1c89cc14531bed988614c8706c24
mt60000 <niruyaku45@gmail.com>
2021-09-22 04:53:50 +0000

    validation追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,12 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items
+
+  validates :postcode, presence: true, length: { is: 7 }
+  validates :address, length: {minimum: 1,  maximum: 100}
+  validates :addressee, length: {minimum: 1,  maximum: 15}
+  validates :postage, presence: true
+
   enum payment_method: {"クレジットカード": 0,"銀行振込": 1}
   enum status: [:入金待ち, :入金確認, :製作中, :発送準備中, :発送済み]
   def status_str
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,6 +1,11 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :item
+
+  validates :count, presence: true
+  validates :price, presence: true
+  validates :item_id, presence: true
+
   enum status: [:impossible, :waiting, :making, :made]
   enum status: [:着手不可, :製作待ち, :製作中, :製作完了]
   def status_str

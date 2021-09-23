af3022c7695fc89bc76fb7f827c86e3a414bbed7
Sugizou0215 <sugizou0215@gmail.com>
2021-09-23 06:27:09 +0000

    絞り込み機能編集中

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,6 +1,31 @@
 class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
   def top
-    @orders = Order.all.order(created_at: "DESC").page(params[:page]).per(8)
+    @allorder = Order.all
+    if params[:order_waiting_value]
+      @orders = Order.where(status: "入金待ち").order(created_at: "DESC").page(params[:page]).per(8)
+    elsif params[:order_preparing_value]
+      @orders = Order.where(status: "入金確認").order(created_at: "DESC").page(params[:page]).per(8)
+    elsif params[:order_making_value]
+      @orders = Order.where(status: "制作中").order(created_at: "DESC").page(params[:page]).per(8)
+    elsif params[:order_sending_value]
+      @orders = Order.where(status: "発送準備中").order(created_at: "DESC").page(params[:page]).per(8)
+    elsif params[:order_complete_value]
+      @orders = Order.where(status: "発送済み").order(created_at: "DESC").page(params[:page]).per(8)
+    elsif params[:order_items_stopping_value]
+      @allorder.each do |order|
+        if order.order_items.where(status: "着手不可").any?
+          @orders << order
+        end
+      end
+    elsif params[:order_items_preparing_value]
+      @orders = @allorder.order_items.where(status: "製作待ち").order(created_at: "DESC").page(params[:page]).per(8)
+    elsif params[:order_items_making_value]
+      @orders = @allorder.order_items.where(status: "製作中").order(created_at: "DESC").page(params[:page]).per(8)
+    elsif params[:order_items_complete_value]
+      @orders = @allorder.order_items.where(status: "製作完了").order(created_at: "DESC").page(params[:page]).per(8)
+    else
+      @orders = Order.all.order(created_at: "DESC").page(params[:page]).per(8)
+    end
   end
 end

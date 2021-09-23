71cb1df5bdcc222c97a917238f10151f2a8eb47b
mt60000 <niruyaku45@gmail.com>
2021-09-23 08:04:44 +0000

    レイアウト調整

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,14 @@
 class SearchesController < ApplicationController
   def word
     @value = params["search"]["value"]
-    @items = Item.search_for(@value).page(params[:id]).per(8)
+    @items = Item.search_for(@value).page(params[:page]).per(8)
     @genres = Genre.all
   end
 
   def genre
     @genre = Genre.find(params[:id])
-    @items = Item.where(genre_id: @genre.id).page(params[:id]).offset(0).per(8)
+    @search_items = Item.where(genre_id: @genre.id)
+    @items = @search_items.page(params[:page]).per(8)
     @genres = Genre.all
   end
 end

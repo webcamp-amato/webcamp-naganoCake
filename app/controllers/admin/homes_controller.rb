40fa533756200b1a28c7c378e59b5dd54c01db55
Sugizou0215 <sugizou0215@gmail.com>
2021-09-23 08:05:12 +0000

    絞り込み完成

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,29 +1,45 @@
 class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
   def top
-    @allorder = Order.all
     if params[:order_waiting_value]
-      @orders = Order.where(status: "入金待ち").order(created_at: "DESC").page(params[:page]).per(8)
+      @orders = Order.where(status: "入金待ち").order(created_at: "DESC").page(params[:page]).per(10)
     elsif params[:order_preparing_value]
-      @orders = Order.where(status: "入金確認").order(created_at: "DESC").page(params[:page]).per(8)
+      @orders = Order.where(status: "入金確認").order(created_at: "DESC").page(params[:page]).per(10)
     elsif params[:order_making_value]
-      @orders = Order.where(status: "制作中").order(created_at: "DESC").page(params[:page]).per(8)
+      @orders = Order.where(status: "制作中").order(created_at: "DESC").page(params[:page]).per(10)
     elsif params[:order_sending_value]
-      @orders = Order.where(status: "発送準備中").order(created_at: "DESC").page(params[:page]).per(8)
+      @orders = Order.where(status: "発送準備中").order(created_at: "DESC").page(params[:page]).per(10)
     elsif params[:order_complete_value]
-      @orders = Order.where(status: "発送済み").order(created_at: "DESC").page(params[:page]).per(8)
+      @orders = Order.where(status: "発送済み").order(created_at: "DESC").page(params[:page]).per(10)
     elsif params[:order_items_stopping_value]
-      @allorder.each do |order|
-        if order.order_items.where(status: "着手不可").any?
-          @orders << order
-        end
+      @items = OrderItem.where(status: "着手不可")
+      @orders = Array.new
+      @items.each do |item|
+        @orders.push(item.order)
       end
+      @orders = @orders.uniq
+      @orders = Kaminari.paginate_array(@orders).page(params[:page]).per(10)
     elsif params[:order_items_preparing_value]
-      @orders = @allorder.order_items.where(status: "製作待ち").order(created_at: "DESC").page(params[:page]).per(8)
+      @items = OrderItem.where(status: "制作待ち")
+      @orders = Array.new
+      @items.each do |item|
+        @orders.push(item.order)
+      end
+      @orders = Kaminari.paginate_array(@orders).page(params[:page]).per(10)
     elsif params[:order_items_making_value]
-      @orders = @allorder.order_items.where(status: "製作中").order(created_at: "DESC").page(params[:page]).per(8)
+      @items = OrderItem.where(status: "制作中")
+      @orders = Array.new
+      @items.each do |item|
+        @orders.push(item.order)
+      end
+      @orders = Kaminari.paginate_array(@orders).page(params[:page]).per(10)
     elsif params[:order_items_complete_value]
-      @orders = @allorder.order_items.where(status: "製作完了").order(created_at: "DESC").page(params[:page]).per(8)
+      @items = OrderItem.where(status: "製作完了")
+      @orders = Array.new
+      @items.each do |item|
+        @orders.push(item.order)
+      end
+      @orders = Kaminari.paginate_array(@orders).page(params[:page]).per(10)
     else
       @orders = Order.all.order(created_at: "DESC").page(params[:page]).per(8)
     end
